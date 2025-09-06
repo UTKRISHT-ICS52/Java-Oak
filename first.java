ca1da1638f11a27900b879ab1ffdbba6c04c512b
UTKRISHT-ICS52 <codewithutkrisht@gmail.com>
2025-09-06 13:03:11 +0530

    Java Programming

--- first.java
+++ first.java
@@ -1,8 +1,51 @@
+
+import java.util.Scanner;
+
 // This is First Java Program 
 
 public class first  {
     public static void main(String[]utkrisht){
         System.out.println("codewithutkrisht");
+    };
+}
+
+class second  {
+    int a=10; // instance variable 
+    static double b=20.5; // static  variable
+    public static void main(String[] args){
+        boolean c = true; // local variable
+        second s=new second();
+        System.out.println(s.a);
+        System.out.println(b);
+        System.out.println(c);
+         
+    }
+}
+
+class second  {
+    int a=10; // instance variable 
+    static double b=20.5; // static  variable
+    public static void main(String[] args){
+        boolean c = true; // local variable
+        second s=new second();
+        System.out.println(s.a+" "+b+" "+c);
+       
+         
+    }
+}
+
+
+public class A {
+    public static void main(String[] args) {
+        int a,b,c; 
+        System.out.println("Enter two numbers :");
+        Scanner s=new  Scanner(System.in);
+        a=s.nextInt();
+        b=s.nextInt();
+        c=a+b;
+        System.out.println("The sum is :"+c);
+        s.close();
+        
     }
 }
 
