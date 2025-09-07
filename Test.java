8f8e0884505aef7bab80ca17076d152f77daaed8
UTKRISHT-ICS52 <codewithutkrisht@gmail.com>
2025-09-07 16:37:47 +0530

    JAVA by Utkrisht

---
+++ Test.java
@@ -0,0 +1,16 @@
+
+/*import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        int a,b,c; 
+        System.out.println("Enter two numbers :");
+        Scanner s=new  Scanner(System.in);
+        a=s.nextInt();
+        b=s.nextInt();
+        c=a+b;
+        System.out.println("The sum is :"+c);
+        s.close();
+    }
+}
+/*
--- first.java
+++ first.java
@@ -1,8 +1,7 @@
 
-import java.util.Scanner;
 
 // This is First Java Program 
-
+/*
 public class first  {
     public static void main(String[]utkrisht){
         System.out.println("codewithutkrisht");
@@ -46,6 +45,35 @@ public class A {
         System.out.println("The sum is :"+c);
         s.close();
         
+    }
+}
+
+
+
+public class first {
+    public static void main(String[] args) {
+        int a,b,c; 
+        System.out.println("Enter two numbers :");
+        Scanner utkrisht =new  Scanner(System.in);
+        a=utkrisht.nextInt();
+        b=utkrisht.nextInt();
+        c=a+b;
+        System.out.println("The sum is :"+c);
+        utkrisht.close();
+    }
+}
+
+*/
+
+public class first{
+    public static void main(String[] args) {
+        int a=37;
+        if(a%2==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
     }
 }
 
